89c55bc6ad4259d9f5b023e5709afd66b586ae57
Gunter IV <larry.gunter-iv@capgemini.com>
2023-07-12 12:33:12 -0400

    added changes

---
+++ front-end/src/components/common/DependentCard.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+function DependentCard({dependent}) {
+  return (
+    <div className="card w-1/3 bg-base-200 shadow-xl py-4">
+        {/* <figure className="px-10 pt-10">
+            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+        </figure> */}
+        <div className="card-body items-center text-center">
+            <h2 className="card-title">{dependent.firstName} {dependent.lastName}</h2>
+
+            <div className="divider" />
+
+            <h2 className="font-bold">Information</h2>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Age</th>
+                            <td>{dependent.age}</td>
+                        </tr>
+                        <tr>
+                            <th>Gender</th>
+                            <td>{dependent.gender}</td>
+                        </tr>
+                        <tr>
+                            <th>Special Needs</th>
+                            <td>{dependent.special_needs}</td>
+                        </tr>
+                        <tr>
+                            <th>Dietary Restrictions</th>
+                            <td>{dependent.dietary_restrictions}</td>
+                        </tr>
+                        <tr>
+                            <th>Allergies</th>
+                            <td>{dependent.allergies}</td>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+
+            <div className="divider"/>
+            <div>
+                <h1 className="font-bold pb-2">Accomodiations</h1>
+                <p className="text-xs font-bold">{dependent.accomodiations}</p>
+            </div>
+
+            <div className="divider"/>
+                <h1 className="font-bold">Medical Accomodiations</h1>
+                <p className="text-xs font-bold">{dependent.medicalInfo}</p>
+
+
+            <div className="divider"/>
+
+            <div className="card-actions">
+                <button className="btn btn-primary">Edit information</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default DependentCard
--- front-end/src/pages/Dependents.js
+++ front-end/src/pages/Dependents.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import UserCard from '../components/common/UserCard'
-
+import { Link } from 'react-router-dom';
+import users from '../mockdata/users.json';
+import DependentCard from '../components/common/DependentCard';
+import dependents from '../mockdata/dependents.json';
 
 function Dependents() {
   return (
     <div>
-      <div className="text-sm breadcrumbs">
-        <ul>
-          <li><a>Home</a></li>
-          <li><a>Dependents</a></li>
-        </ul>
-      </div>
-        <h1>Dependents</h1>
-        <UserCard />
+
+        <h1 className="text-3xl text-center font-bold py-6">Dependents</h1>
+
+
+        {/* <div className="mt-3 grid grid-cols-1 gap-6 md:grid-cols-2">
+            {users.dependents.length ?
+                users.dependents.map((dependent, index) =>  {
+                    return (
+                        <UserCard key={index} dependent={dependent}></UserCard>
+                    )
+                })
+            : <p>No Dependents Yet</p>}
+        </div> */}
+
+
+        <div className="grid grid-flow-col">
+            {dependents.dependents.map((dependent, index) =>{
+                return <DependentCard key={index} dependent={dependent}/>
+            })}
+        </div>
+        
+
     </div>
   )
 }
