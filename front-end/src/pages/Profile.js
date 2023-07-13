a967c8102b7c3b90a724e62ba857923c9c0f7a3d
Gunter IV <larry.gunter-iv@capgemini.com>
2023-07-13 10:04:59 -0400

    added dependencies

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import axios from 'axios';
+
 
 function Profile() {
   return (
-    <div>
-        <h1 className="font-bold text-center">Profile Page</h1>
+    <div className="flex flex-col justify-center">
+        <h1 className="font-bold py-10">Profile Page</h1>
+
+        <div className="card w-96 bg-base-100 shadow-xl">
+        
+            <div className="card-body">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end flex justify-center">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+
+        <div className="card w-96 bg-base-100 shadow-xl">
+        
+            <div className="card-body">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end flex justify-center">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
